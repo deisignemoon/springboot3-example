452f765c769322450e67f727edbac0d24f1499e4
xiacong <946933678@qq.com>
2024-06-13 08:39:14 +0800

    重写自定义requestWrapper；

--- common/src/main/java/com/xiacong/common/filter/RequestWrapper.java
+++ common/src/main/java/com/xiacong/common/filter/RequestWrapper.java
@@ -4,44 +4,49 @@ import jakarta.servlet.ReadListener;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequestWrapper;
+import org.apache.commons.io.IOUtils;
+
 import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 
 /**
- * http请求数据复制bean
- * http请求数据只能读取一次，读取后即关闭，使用本；类可以进行数据复制，解决该问题
- * @author zhuquanwen
- * @vesion 1.0
- * @date 2022/2/21 15:55
- * @since jdk1.8
+ * @Description: 对HttpServletRequest进行重写，
+ * 1、用来接收application/json参数数据类型，即@RequestBody注解标注的参数,解决多次读取问题
+ * 2、用来解决注解@RequestParam通过POST/PUT/DELETE/PATCH方法传递参数，解决多次读取问题
+ * 首先看一下springboot控制器三个注解：
+ * 1、@PathVariable注解是REST风格url获取参数的方式，只能用在GET请求类型，通过getParameter获取参数
+ * 2、@RequestParam注解支持GET和POST/PUT/DELETE/PATCH方式，Get方式通过getParameter获取参数和post方式通过getInputStream或getReader获取参数
+ * 3、@RequestBody注解支持POST/PUT/DELETE/PATCH，可以通过getInputStream和getReader获取参数
+ * @create: 2020/8/19
  */
 public class RequestWrapper extends HttpServletRequestWrapper {
-    private final byte[] body;
+    //参数字节数组
+    private byte[] requestBody;
+    //Http请求对象
+    private HttpServletRequest request;
 
     public RequestWrapper(HttpServletRequest request) throws IOException {
         super(request);
-        // 获取requestbody中的数据
-        body = getBodyString(request).getBytes(StandardCharsets.UTF_8);
+        this.request = request;
     }
 
-    @Override
-    public BufferedReader getReader() throws IOException {
-        return new BufferedReader(new InputStreamReader(getInputStream()));
-    }
-
+    /**
+     * @return
+     * @throws IOException
+     */
     @Override
     public ServletInputStream getInputStream() throws IOException {
-        // 定义内存中的输入流
-        final ByteArrayInputStream bais = new ByteArrayInputStream(body);
+        /**
+         * 每次调用此方法时将数据流中的数据读取出来，然后再回填到InputStream之中
+         * 解决通过@RequestBody和@RequestParam（POST方式）读取一次后控制器拿不到参数问题
+         */
+        if (null == this.requestBody) {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            IOUtils.copy(request.getInputStream(), baos);
+            this.requestBody = baos.toByteArray();
+        }
 
+        final ByteArrayInputStream bais = new ByteArrayInputStream(requestBody);
         return new ServletInputStream() {
-
-            @Override
-            public int read() throws IOException {
-                // 使用内存输入流读取数据
-                return bais.read();
-            }
 
             @Override
             public boolean isFinished() {
@@ -54,43 +59,23 @@ public class RequestWrapper extends HttpServletRequestWrapper {
             }
 
             @Override
-            public void setReadListener(ReadListener readListener) {
+            public void setReadListener(ReadListener listener) {
 
+            }
+
+            @Override
+            public int read() {
+                return bais.read();
             }
         };
     }
 
-    public static String getBodyString(HttpServletRequest request) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        InputStream inputStream = null;
-        BufferedReader reader = null;
-        try {
-            inputStream = request.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
-            String line = "";
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return sb.toString();
+    public byte[] getRequestBody() {
+        return requestBody;
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(this.getInputStream()));
     }
 }
-
-
